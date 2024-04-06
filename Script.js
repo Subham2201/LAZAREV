c5b3fb8d62c185a7b507d08253dc8b175a437cff
Subham2201 <subham.saha22012@gmail.com>
2024-04-06 19:25:12 +0530

    Pages Added

--- Script.js
+++ Script.js
@@ -31,4 +31,65 @@ nav.addEventListener("mouseleave", function(){
     })
 })
 }
-navAnimation()
+// navAnimation()
+
+function page2(){
+    var relem= document.querySelectorAll(".right-elem")
+relem.forEach(function(elem){
+        elem.addEventListener("mouseenter", function(){
+            gsap.to(elem.childNodes[3],{
+                opacity:1,
+                scale:1
+            })
+        })
+        elem.addEventListener("mouseleave", function(){
+            gsap.to(elem.childNodes[3],{
+                opacity:0,
+                scale:0
+            })
+        })
+        elem.addEventListener("mousemove", function(dets){
+            gsap.to(elem.childNodes[3],{
+                x:dets.x- elem.getBoundingClientRect().x-50,
+                y:dets.y- elem.getBoundingClientRect().y-190
+            })
+    })
+})
+}
+page2();
+
+
+function page3Video(){
+    var pcen= document.querySelector(".page3-center");
+var video= document.querySelector("#page3 video");
+
+pcen.addEventListener("click", function(){
+    video.play()
+    gsap.to(video,{
+        transform: "scaleX(1) scaleY(1)",
+        opacity:1,
+        borderRadius:0
+    })
+})
+video.addEventListener("click", function(){
+    video.pause()
+    gsap.to(video,{
+        transform: "scaleX(0.7) scaleY(0)",
+        opacity:0,
+        borderRadius:"30px"
+    })
+})
+}
+page3Video();
+
+var sec= document.querySelectorAll(".sec-right ");
+sec.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        elem.childNodes[3].style.opacity=1
+        elem.childNodes[3].play()
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.childNodes[3].style.opacity=0
+        elem.childNodes[3].load()
+    })
+});
